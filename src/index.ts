4ad51173507708e8868bc330cd67765b6bdfa7c1
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2022-12-16 18:03:46 +0300

    feat: start ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 import "./scss/base.css";
+import { putProducts } from "./ts/products";
+
+const page = document.body;
+
+switch(page.id){
+    case 'index':
+        putProducts();
+        break;
+}
