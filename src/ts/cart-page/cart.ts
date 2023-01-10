5e5c91d788fd5ca65763ee22af4d32be74a18545
Elena-Amelia <amelia013@ya.ru>
2023-01-10 23:55:09 +0400

    fix: fix cart-products click

--- src/ts/cart-page/cart.ts
+++ src/ts/cart-page/cart.ts
@@ -15,19 +15,4 @@ export function startCart() {
   changeTotalItemsAndMoney(cartProducts);
   createAppliedPromo();
   changePromoCodeMoney();
-
-  const itemInfo = document.querySelectorAll<HTMLElement>(".item__info");
-  itemInfo.forEach((item) => {
-    item.addEventListener("click", goToDetailsPage);
-  });
-
 }
-
-  function goToDetailsPage(e: Event) {
-    const target = e.target as HTMLElement;
-    const product = target?.closest(".cart-products__item_wrapper");
-    if (product) {
-      localStorage.setItem("productDetails", String(product.id));
-      window.location.href = "./details.html";
-    }
-  }
---
+++ src/ts/cartProductsClick.ts
@@ -0,0 +1,16 @@
+export function cartProductsClick() {
+    const itemInfo = document.querySelectorAll<HTMLElement>(".item__info");
+    itemInfo.forEach((item) => {
+    item.addEventListener("click", goToDetailsPage);
+    });
+  }
+
+
+function goToDetailsPage(e: Event) {
+    const target = e.target as HTMLElement;
+    const product = target?.closest(".cart-products__item_wrapper");
+    if (product) {
+      localStorage.setItem("productDetails", String(product.id));
+      window.location.href = "./details.html";
+    }
+}
