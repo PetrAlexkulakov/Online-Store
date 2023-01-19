6eeacd6535b97e0843e6fcf33cf5c42108ff0cea
Elena-Amelia <amelia013@ya.ru>
2023-01-19 22:08:09 +0400

    fix: fix usedPromocodes.ts

--- src/ts/cart-page/usePromoCode.ts
+++ src/ts/cart-page/usePromoCode.ts
@@ -22,30 +22,37 @@ if (localStore) {
 
 function showPromoCodes() {
   if (!promoCodeInput || !foundPromo) return;
-  const localStorPromoCodes = localStorage.getItem("OSpromoCodes");
-  outer: for (let i = 0; i < availablePromoCodes.length; i++) {
+
+  for (let i = 0; i < availablePromoCodes.length; i++) {
     if (promoCodeInput.value.toUpperCase() === availablePromoCodes[i].id) {
       foundPromo.style.display = "block";
-      if (!localStorPromoCodes) {
-        foundPromo.innerHTML = `${availablePromoCodes[i].name} - ${availablePromoCodes[i].disc}% <span class="applied-codes__span found-promo__btn_add">ADD</span>`;
-        break outer;
+      if (checkUsedPromoCode()) {
+        foundPromo.innerHTML = `${availablePromoCodes[i].name} - ${availablePromoCodes[i].disc}%`;
+        break;
       } else {
-        let usedPromoCodes: AllPromoCodes[] = [];
-        usedPromoCodes = JSON.parse(localStorPromoCodes);
-        for (let j = 0; j < usedPromoCodes.length; j++) {
-          if (promoCodeInput.value.toUpperCase() === usedPromoCodes[j].id) {
-            foundPromo.innerHTML = `${usedPromoCodes[j].name} - ${usedPromoCodes[j].disc}%`;
-            break outer;
-          } else {
-            foundPromo.innerHTML = `${availablePromoCodes[i].name} - ${availablePromoCodes[i].disc}% <span class="applied-codes__span found-promo__btn_add">ADD</span>`;
-            break outer;
-          }
-        }
+        foundPromo.innerHTML = `${availablePromoCodes[i].name} - ${availablePromoCodes[i].disc}%
+        <span class="applied-codes__span found-promo__btn_add">ADD</span>`;
+        break;
       }
     } else {
       foundPromo.style.display = "none";
     }
   }
+}
+
+function checkUsedPromoCode(): boolean | undefined {
+  const localStorPromoCodes = localStorage.getItem("OSpromoCodes");
+  if (!localStorPromoCodes) return;
+  const usedPromoCodes: AllPromoCodes[] = JSON.parse(localStorPromoCodes);
+  let isUsed = false;
+
+  for (let i = 0; i < usedPromoCodes.length; i++) {
+    if (usedPromoCodes[i].id === promoCodeInput?.value.toUpperCase()) {
+      isUsed = true;
+      break;
+    }
+  }
+  return isUsed;
 }
 
 function addPromoCode(e: Event) {
@@ -90,26 +97,24 @@ function applyPromoCode(target: HTMLElement) {
 export function createAppliedPromo() {
   const localStorPromoCodes = localStorage.getItem("OSpromoCodes");
   if (!appliedCodes || !localStorPromoCodes) return;
-  if (localStorPromoCodes.length === 0) return;
+
   appliedCodes.style.display = "block";
   appliedCodes.innerHTML = "";
   const title = document.createElement("h3");
   title.innerHTML = "Applied codes";
   appliedCodes.appendChild(title);
 
-  let usedPromoCodes: AllPromoCodes[] = [];
-  if (localStorPromoCodes) {
-    usedPromoCodes = JSON.parse(localStorPromoCodes);
+  const usedPromoCodes: AllPromoCodes[] = JSON.parse(localStorPromoCodes);
 
-    usedPromoCodes.forEach((item) => {
-      const appliedPromo = document.createElement("div");
-      appliedPromo.classList.add("applied-promo");
-      appliedPromo.setAttribute("id", `${item.id}`);
-      appliedPromo.innerHTML = `${item.name} - ${item.disc}% - <span class="applied-codes__span promo__btn_drop">DROP</span>`;
-      appliedCodes.appendChild(appliedPromo);
-    });
-  }
+  usedPromoCodes.forEach((item) => {
+    const appliedPromo = document.createElement("div");
+    appliedPromo.classList.add("applied-promo");
+    appliedPromo.setAttribute("id", `${item.id}`);
+    appliedPromo.innerHTML = `${item.name} - ${item.disc}% - <span class="applied-codes__span promo__btn_drop">DROP</span>`;
+    appliedCodes.appendChild(appliedPromo);
+  });
 }
+
 function deletePromoCode(target: HTMLElement) {
   const parent = target.closest(".applied-promo");
   const parentId = parent?.getAttribute("id");
