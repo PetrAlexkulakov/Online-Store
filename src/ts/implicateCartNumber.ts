015f88ec1a479a0d47c4311db70c28b4f08c979f
Elena-Amelia <amelia013@ya.ru>
2023-01-26 18:27:28 +0400

    refactor: simplify implicateCartNumber

--- src/ts/implicateCartNumber.ts
+++ src/ts/implicateCartNumber.ts
@@ -2,16 +2,15 @@ import { CartLocalStore } from "./cart-page/cartTypes";
 
 export function implicateCartNumber(){
     const cartNumber = document.querySelector('.header__cart__number__content') as HTMLDivElement;
-    let cartProducts: CartLocalStore[] = [];
     const localStore = localStorage.getItem('cartProducts');
-    if (localStore){
-        cartProducts = JSON.parse(localStore);
+    if (localStore) {
+        const cartProducts: CartLocalStore[] = JSON.parse(localStore);
         const cartExample = cartProducts[0];
         const productNumbers: number = cartProducts.reduce(
             (sum: number, item: typeof cartExample) => sum + item.count, 0);
 
         cartNumber.textContent = String(productNumbers);
     } else {
-        cartNumber.textContent = String(cartProducts.length);
+        cartNumber.textContent = '0';
     }
 }
